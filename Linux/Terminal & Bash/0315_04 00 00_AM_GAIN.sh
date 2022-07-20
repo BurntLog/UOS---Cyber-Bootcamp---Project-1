efd6a71b633f8db21b187c1ca976ba8d7b40a6a8
Peter <peterblightan@hotmail.com>
2022-07-21 03:47:44 +1000

    First commit

---
+++ Linux/Terminal & Bash/0315_04 00 00_AM_GAIN.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+awk '{print $1, $2, $5, $6}' 0315_Dealer_schedule | grep "04:00:00 AM"
+
---
+++ Linux/Terminal & Bash/roulette_dealer_finder_by_time.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+awk '{print $1, $2, $5, $6}' $1_Dealer_schedule | grep $2:00:00 | grep $3M
+
